62a7b6debfa48c64cba2c8d41d26c0a0fe68db3f
Luis Mendoza <lumendoza@bncr.fi.cr>
2023-11-03 23:29:58 -0400

    Configure ocelot path

--- Practice.Gateway/Program.cs
+++ Practice.Gateway/Program.cs
@@ -5,11 +5,15 @@ using Practice.Gateway.Aggregator;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
+//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration).
+    AddSingletonDefinedAggregator<UsersPostsAggregator>();
 // Add services to the container.
-builder.Services.AddOcelot()
-    .AddSingletonDefinedAggregator<UsersPostsAggregator>();
+//builder.Services.AddOcelot()
+    //.AddSingletonDefinedAggregator<UsersPostsAggregator>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
